29ab31e70441180a479d2ee20adbba8ff1ed6eff
Renan Tatsuo (kodi) <tatsuorenan@gmail.com>
2019-03-10 13:15:49 -0300

    update classNames, add transition and focus to MenuItem

--- client/src/Components/MenuItem/index.jsx
+++ client/src/Components/MenuItem/index.jsx
@@ -5,21 +5,21 @@ import { convertFloatToMoney } from '../../Helpers'
 import './MenuItem.scss'
 
 const MenuItem = ({ photo, title, desc, price }) => (
-  <div className="menu_item">
-    <div className="menu_item--photo">
+  <button type="button" className="menu_item">
+    <div className="menu_item__photo">
       <img src={photo} alt={title} />
     </div>
-    <div className="menu_item--info">
-      <div className="menu_item--info__title">{title}</div>
-      <div className="menu_item--info__desc">{desc}</div>
-      <div className="menu_item--info__price">
+    <div className="menu_item__info">
+      <div className="menu_item__info--title">{title}</div>
+      <div className="menu_item__info--desc">{desc}</div>
+      <div className="menu_item__info--price">
         R$ {convertFloatToMoney(price)}
       </div>
     </div>
-    <div className="menu_item--cart">
+    <div className="menu_item__cart">
       <AddShoppingCartOutlined />
     </div>
-  </div>
+  </button>
 )
 
 export default MenuItem
