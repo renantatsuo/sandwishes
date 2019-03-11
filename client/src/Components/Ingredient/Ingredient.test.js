5d6ad30a6fb347b0bd1191458b9b122e3242f6e5
Renan Tatsuo (kodi) <tatsuorenan@gmail.com>
2019-03-10 21:42:50 -0300

    create Ingredient component

---
+++ client/src/Components/Ingredient/Ingredient.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Ingredient from './index'
+
+describe('Test Ingredient component', () => {
+  it('Should render with no errors', () => {
+    const menuItem = shallow(<Ingredient />)
+
+    expect(menuItem.find('.ingredient__info--price').text()).toEqual('R$ 0,00')
+    expect(menuItem).toMatchSnapshot()
+  })
+})
---
+++ client/src/Components/Ingredient/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import { Add, Remove } from '@material-ui/icons'
+
+import { convertFloatToMoney } from '../../Helpers'
+import './Ingredient.scss'
+
+const Ingredient = ({
+  addIngredient,
+  id,
+  photo,
+  price,
+  removeIngredient,
+  quantity,
+  title
+}) => {
+  const [counter, setCounter] = useState(0)
+
+  const handleAdd = () => {
+    addIngredient(id)
+    setCounter(counter + 1)
+  }
+  const handleRemove = () => {
+    removeIngredient(id)
+    setCounter(counter - 1)
+  }
+
+  useEffect(() => {
+    setCounter(quantity)
+  }, [quantity])
+
+  return (
+    <div className="ingredient">
+      <div className="ingredient__photo">
+        <img src={photo} alt={title} />
+      </div>
+      <div className="ingredient__info">
+        <div className="ingredient__info--title">{title}</div>
+        <div className="ingredient__info--price">
+          R$ {convertFloatToMoney(price)}
+        </div>
+      </div>
+      <div className="ingredient__cart">
+        {counter === 0 ? (
+          <button type="button" disabled>
+            <Remove />
+          </button>
+        ) : (
+          <button type="button" onClick={handleRemove}>
+            <Remove />
+          </button>
+        )}
+        {counter}
+        <button type="button" onClick={handleAdd}>
+          <Add />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Ingredient
