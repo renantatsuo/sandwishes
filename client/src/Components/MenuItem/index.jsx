a15c16bbd223c76dca86a20a02af51c2660e5761
renantatsuo <tatsuorenan@gmail.com>
2020-10-06 22:31:06 -0300

    Update MenuItem component price

--- client/src/Components/MenuItem/index.jsx
+++ client/src/Components/MenuItem/index.jsx
@@ -1,26 +1,25 @@
+import { AddShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { AddShoppingCartOutlined } from '@material-ui/icons'
-
 import { convertFloatToMoney } from '../../helpers'
 import './MenuItem.scss'
 
-const MenuItem = ({ desc, photo, price, title, to }) => (
-  <Link to={to} className="menu_item">
-    <div className="menu_item__photo">
-      <img src={photo} alt={title} />
-    </div>
-    <div className="menu_item__info">
-      <div className="menu_item__info--title">{title}</div>
-      <div className="menu_item__info--desc">{desc}</div>
-      <div className="menu_item__info--price">
-        R$ {convertFloatToMoney(price)}
-      </div>
-    </div>
-    <div className="menu_item__cart">
-      <AddShoppingCartOutlined />
-    </div>
-  </Link>
-)
+export default function MenuItem({ desc, photo, price, title, to }) {
+  const formattedPrice = `R$ ${convertFloatToMoney(price)}`
 
-export default MenuItem
+  return (
+    <Link to={to} className="menu_item">
+      <div className="menu_item__photo">
+        <img src={photo} alt={title} />
+      </div>
+      <div className="menu_item__info">
+        <div className="menu_item__info--title">{title}</div>
+        <div className="menu_item__info--desc">{desc}</div>
+        <div className="menu_item__info--price">{formattedPrice}</div>
+      </div>
+      <div className="menu_item__cart">
+        <AddShoppingCartOutlined />
+      </div>
+    </Link>
+  )
+}
