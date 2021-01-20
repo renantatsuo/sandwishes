f69282735489f63b842fd008f103525d0776db80
renantatsuo <tatsuorenan@gmail.com>
2021-01-20 00:03:17 -0300

    Move HTTP module to Lib

--- client/src/Lib/Http/index.js
+++ client/src/Lib/Http/index.js
@@ -4,8 +4,8 @@ import axios from 'axios'
  * Abstraction layer for HTTP requests.
  */
 const Http = {
-  get: async (url) => {
-    const response = await axios.get(url)
+  get: async (url, options) => {
+    const response = await axios.get(url, options)
     return {
       data: response.data,
     }
